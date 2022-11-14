a7e2bafb75335024b7089d4ebfefece228142700
your_github_username <msalamonpaven18@jcu.edu>
2022-11-15 00:12:45 +0200

    Milestone 2

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,14 +1,65 @@
 import "./App.css";
 import { useState } from "react";
-import NoteCreatorForm from "./NoteCreatorForm";
-import NoteList from "./NoteList";
+
+function NoteBox(props) {
+  const { date, myKey, deleteNote } = props;
+  return (
+    <div className="note-box">
+      <h3>{date}</h3>
+      <p>Example Note</p>
+      <button
+        onClick={() => {
+          deleteNote(myKey);
+        }}
+      >
+        x
+      </button>
+    </div>
+  );
+}
+
+function NoteCreatorForm({ addNote }) {
+  const submitNote = (e) => {
+    e.preventDefault();
+    addNote();
+  };
+  return (
+    <form className="note-creator-form">
+      <textarea
+        className="note-input-area"
+        type="text"
+        placeholder="Type your Note here..."
+      ></textarea>
+      <button onClick={submitNote} type="submit" className="note-submit-button">
+        +
+      </button>
+    </form>
+  );
+}
 
 function App() {
   const [dates, createDate] = useState([]);
   const addNote = () => {
     const dateCreator = new Date();
-    const newDate = dateCreator.toString();
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    };
+    const newDate = dateCreator.toLocaleDateString(undefined, options);
     createDate([...dates, newDate]);
+  };
+  const removeNote = (myKey) => {
+    const deletePopUp = window.confirm(
+      "Are you sure you want to delete this note?"
+    );
+    if (deletePopUp == true) {
+      const dateDuplicate = [...dates];
+      dateDuplicate.splice(myKey, 1);
+      createDate(dateDuplicate);
+    }
   };
   return (
     <div className="App">
@@ -16,7 +67,18 @@ function App() {
         <h1>Notes App</h1>
       </header>
       <NoteCreatorForm addNote={addNote} />
-      <NoteList noteCreationDates={dates} />
+      <div className="note-grid-box">
+        {dates.map((date, key) => {
+          return (
+            <NoteBox
+              date={date}
+              key={key}
+              myKey={key}
+              deleteNote={removeNote}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
