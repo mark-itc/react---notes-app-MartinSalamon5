cbfb94d7d92a2062fedd9e4bfe142033e445752c
your_github_username <msalamonpaven18@jcu.edu>
2022-11-15 22:13:08 +0200

    Milestone 4

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,19 +1,29 @@
 import "./App.css";
 import { useState } from "react";
+import Modal from "react-modal";
 
 function NoteBox(props) {
-  const { date, myKey, deleteNote } = props;
+  const { date, noteText, myKey, deleteNote, noteTitle, openModal } = props;
   return (
-    <div className="note-box">
-      <h3>{date}</h3>
-      <p>Example Note</p>
+    <div className="note-box-wrapper">
       <button
+        className="delete-button"
         onClick={() => {
           deleteNote(myKey);
         }}
       >
         x
       </button>
+      <div
+        className="note-box"
+        onClick={() => {
+          openModal(date, noteTitle, noteText);
+        }}
+      >
+        <p className="note-date">{date}</p>
+        <h5>{noteTitle}</h5>
+        <p>{noteText}</p>
+      </div>
     </div>
   );
 }
@@ -21,23 +31,41 @@ function NoteBox(props) {
 function NoteCreatorForm({ addNote }) {
   const submitNote = (e) => {
     e.preventDefault();
-    addNote();
+    addNote("Note Title");
   };
   return (
     <form className="note-creator-form">
-      <textarea
-        className="note-input-area"
-        type="text"
-        placeholder="Type your Note here..."
-      ></textarea>
+      <div className="inputs-box">
+        <input className="title-input" placeholder="Title..."></input>
+        <textarea
+          className="note-input-area"
+          type="text"
+          placeholder="Type your Note here..."
+        ></textarea>
+      </div>
       <button onClick={submitNote} type="submit" className="note-submit-button">
         +
       </button>
     </form>
   );
 }
+Modal.setAppElement("#root");
 
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalDate, setModalDate] = useState();
+  const [modalTitle, setModalTitle] = useState();
+  const [modalText, setModalText] = useState();
+
+  function openModal(date, noteTitle, noteText) {
+    setIsOpen(true);
+    setModalDate(date);
+    setModalTitle(noteTitle);
+    setModalText(noteText);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
   const [dates, createDate] = useState([]);
   const addNote = () => {
     const dateCreator = new Date();
@@ -66,6 +94,15 @@ function App() {
       <header className="App-header">
         <h1>Notes App</h1>
       </header>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal">
+        <button className="modal-close-button" onClick={closeModal}>
+          x
+        </button>
+        <p>{modalDate}</p>
+        <h2>{modalTitle}</h2>
+        <p>{modalText}</p>
+      </Modal>
+
       <NoteCreatorForm addNote={addNote} />
       <div className="note-grid-box">
         {dates.map((date, key) => {
@@ -75,6 +112,9 @@ function App() {
               key={key}
               myKey={key}
               deleteNote={removeNote}
+              noteTitle={"Note Title"}
+              noteText={"Example Note"}
+              openModal={openModal}
             />
           );
         })}
